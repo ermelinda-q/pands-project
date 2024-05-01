647a1425def9254ca206a3efaeec59dcb23f4328
ermelinda-q <ermelinda.qejvani@gmail.com>
2024-05-01 20:42:21 +0100

    returning to project

---
+++ analysis-functions.py
@@ -0,0 +1,5 @@
+# This file contains functions for main file analysis.txt
+
+# Clean text function
+def clean_text(text):
+    
--- analysis.py
+++ analysis.py
@@ -15,3 +15,34 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 import sys
 import os
+
+# Creating a text file to write the data from running the program
+# Using 'w' parameter we make sure that if the file doesn't exist it is created. 
+# It will also empty the content of the file every time the program is run.
+my_file = open("analysis.txt", "w")
+my_file.write("This file contains my analysis of the Iris dataset.\n\n")
+# my_file.close
+
+# Loading the data to the dataframe pd.
+df = pd.read_csv('iris.csv')
+
+
+# checking if we can write info to our text file.
+my_file.write(df.head().to_string())
+my_file.write("\n\n")
+my_file.write(df.tail().to_string())
+
+# print that all went well.
+print("writing in the file was successful! ")
+
+# append if in the text file.
+
+
+
+
+
+
+
+# references:
+
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
